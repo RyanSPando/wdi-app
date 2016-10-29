f26a8e5ad7df66cbb656e178efd2b826e8d42d37
Ryan "archaen" Pando <vretha@gmail.com>
2016-10-29 12:20:18 -0600

    file is being selected.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-// Check for the various File API support.
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-  // Great success! All the File APIs are supported.
-  console.log('all good');
-} else {
-  console.log('The File APIs are not fully supported in this browser.');
-}
+function handleFileSelect(evt) {
+    var file = evt.target.files[0]; // FileList object
+    console.log(file);
+  }
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
