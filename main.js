52969defe452c2eaeae2b92322810eae59f5ca64
Ryan "archaen" Pando <vretha@gmail.com>
2016-10-29 12:57:29 -0600

    File is being outputted as json to dom.  Detecting if JSON.

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-function handleFileSelect(evt) {
-    var file = evt.target.files[0]; // FileList object
-    console.log(file);
-  }
+window.onload = function() {//onload iife
 
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+  //select input field and output area
+	var jsonInput = document.getElementById('jsonInput');
+	var htmlDisplayArea = document.getElementById('htmlDisplayArea');
+
+  //listen for file input
+	jsonInput.addEventListener('change', function(e) {
+    //grab file on input
+		var json = jsonInput.files[0];
+    //get type of file
+		var fileType = /json.*/;
+
+    //if file is json, read file
+		if (json.type.match(fileType)) {
+			var reader = new FileReader();
+
+			reader.onload = function(e) {
+        //display stuff from file
+				htmlDisplayArea.innerText = reader.result;
+			};
+      //r
+			reader.readAsText(json);
+		} else {
+			htmlDisplayArea.innerText = "File not supported!";
+		}
+	});
+};
